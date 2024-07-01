763bca13a9af0ad1f6d030f535a3593c383f2129
bongjooncha <cbj4133@naver.com>
2024-07-01 21:36:33 +0900

    너구리 dfs 방식 해결, 근데 is not 0이 없을떄 왜 1이아니라 2 나오는지 확인 필요

--- baekjoon/silver2/raccon.py
+++ baekjoon/silver2/raccon.py
@@ -7,9 +7,13 @@ Tree=[ [] for _ in range(N+1) ]
 [Tree[a].append((b, c)) for a, b, c in graphs]
 [Tree[b].append((a, c)) for a, b, c in graphs]
 
+#Node: 시작점, count: 
 def DFS(Node,count):
+    # 최대 길이
     global total
+    # 방문 처리
     visit[Node]=True
+    # i: 도착지, j 거리
     for i,j in Tree[Node]:
         if not visit[i]:
             total=max(total, count+j)
--- try.py
+++
@@ -1,5 +0,0 @@
-a = [1,3,0,2]
-
-for i in a:
-    if i is not 0:
-        print(i)
