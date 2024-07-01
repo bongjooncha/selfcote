11905777c5ff2336851bff75fc3e12f5a71dbb8f
hiddenbebb <cbj4133@naver.com>
2024-07-02 00:18:16 +0900

    너구리99 완벽 풀이

--- baekjoon/silver2/racon99_dfs.py
+++ baekjoon/silver2/racon99_dfs.py
@@ -5,17 +5,24 @@ sys.setrecursionlimit(10**9)
 N=int(input())
 graphs = [list(map(int, input().split())) for _ in range(N-1)]
 Tree=[ [] for _ in range(N+1) ]
+
 [Tree[a].append((b, c)) for a, b, c in graphs]
 [Tree[b].append((a, c)) for a, b, c in graphs]
 
-visited = [0] * (N+1)
+distances = [0] * (N+1)
 
+# graph: (목적지, 거리), v:시작점
 def dfs(graph, v):
-    for end, distance in graph[v]:
-        if visited[end] == 0:
-            visited[end] = visited[v] + distance
-            dfs(graph, end)
-        else:
-            visited[end] = min(visited[end],visited[v]+distance)
+        for end, distance in graph[v]:
+            if distances[end] is 0  and end is not 1:
+                distances[end] = distances[v] + distance
+                dfs(graph, end)
+                
 dfs(Tree,1)
-print(max(visited))
+print(max(distances))
+
+
+# 4
+# 1 4 2
+# 1 2 1
+# 4 3 1
