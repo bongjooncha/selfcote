731f50ccf24bbdce3d7c6b5755193a20ee269b79
hiddenbebb <cbj4133@naver.com>
2024-08-06 20:14:06 +0900

    24.08.06(baek silver3)

--- try.py
+++ try.py
@@ -1,16 +1,1 @@
-from itertools import combinations
-
-N = int(input())
-a_list = list(map(int,input().split()))
-a_list.remove(0)
-a_list.sort()
-
-# comb는 무작위
-for comb in combinations(a_list,N):
-    new = []
-    for i in range(1,len(comb)+1):
-        for make in combinations(comb,i):
-            new.append(sum(make))
-    new.sort()
-    if a_list == new:
-        print(comb)
+print(int(5/2))
