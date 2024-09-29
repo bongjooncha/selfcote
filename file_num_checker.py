7680f6afe13716f85f4c403ff81e30ad01592ff4
hiddenbebb <cbj4133@naver.com>
2024-09-29 11:57:47 +0900

    24.09.29 백siv2

--- file_num_checker.py
+++ file_num_checker.py
@@ -1,5 +1,5 @@
 import os
 
 # 현재 디렉토리의 파일 목록 가져오기
-files = os.listdir("./softeer/lv2_2")  # 혹은 os.listdir('./경로') 현재 디렉토리에 대해 
-print(len(files))
+files = os.listdir("./baekjoon")  # 혹은 os.listdir('./경로') 현재 디렉토리에 대해 
+print(files)
