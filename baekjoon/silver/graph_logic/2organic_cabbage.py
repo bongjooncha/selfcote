e94132cbe11d882be271eae14bb9070d6a7c2f4b
hiddenbebb <cbj4133@naver.com>
2024-10-08 14:22:07 +0900

    24.10.08 baek 5문제

---
+++ baekjoon/silver/graph_logic/2organic_cabbage.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/1012
+import sys
+sys.setrecursionlimit(10**9)
+
+T = int(input())
+
+def dfs(x,y):
+    if 0<=x<M and 0<=y<N and graph [y][x] == 1:
+        graph[y][x] = 0
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x,y-1)
+        dfs(x,y+1)
+
+for i in range(T):
+    M, N, K = map(int, input().split())
+    graph = [[0]*M for _ in range(N)]
+    for i in range(K):
+        x,y = map(int, input().split())
+        graph[y][x] = 1
+
+    ans =0
+    for i in range(M):
+        for j in range(N):
+            if graph[j][i] ==1:
+                dfs(i, j)
+                ans +=1
+    print(ans)
+    
